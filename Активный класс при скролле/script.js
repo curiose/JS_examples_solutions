8a8874b24c3389e611c8fffcdb40277d845cfb87
curiose <attain06@gmail.com>
2022-05-19 18:00:53 +0300

    Активный класс при скролле

---
+++ Активный класс при скролле/script.js
@@ -0,0 +1,17 @@
+// определяем scroll
+window.addEventListener('scroll', () => {
+   // текущее положение скролла
+   let scrollDistance = window.scrollY;
+
+   document.querySelectorAll('.section').forEach((el, i) => {
+      if (el.offsetTop - document.querySelector('.nav').clientHeight <= scrollDistance) {
+         document.querySelectorAll('.nav a').forEach((el => {
+            if (el.classList.contains('active')) {
+               el.classList.remove('active');
+            }
+         }));
+
+         document.querySelectorAll('.nav li')[i].querySelector('a').classList.add('active');
+      }
+   });
+});
